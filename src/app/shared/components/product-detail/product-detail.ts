1fba361c6330d561773fb5c8d924b3f3be8ebadc
Anne Manthey <anne.manthey@pm.me>
2026-07-20 15:54:48 +0200

    feat: set product detail as service

--- src/app/shared/components/product-detail/product-detail.ts
+++ src/app/shared/components/product-detail/product-detail.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Products } from '../../services/products';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,21 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetail {
   private route = inject(ActivatedRoute);
+  productService = inject(Products)
+
+
+  detail = {
+    name: 'n/a',
+    description: 'n/a',
+    specs: 'n/a',
+    stock: 0,
+    price: 0
+  };
 
   ngOnInit() {
     let currentName = this.route.snapshot.paramMap.get('name');
-    if (currentName) this.detail.name = currentName;
+    if (currentName) this.productService.setProductDetailByName(currentName);
+    this.detail = this.productService.productdetail;
   }
-
-  detail = {
-    name: 'Gaming Maus',
-    description: `Eine ergonomische Gaming-Maus mit hoher Praezision und einstellbarer
-DPI. Ideal fuer FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortable
-Seitentasten fuer schnelles Reagieren.`,
-    specs: 'dpi: 6400, cable length: 1.8m, color: Schwarz',
-    stock: 120,
-    price: 2500000,
-  };
 
   deleteDetail() {
     this.detail.name = '';
