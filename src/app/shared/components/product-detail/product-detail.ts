183766e95f05187be6d7a2f5bc614b4b94395e13
Anne Manthey <anne.manthey@pm.me>
2026-07-20 07:07:48 +0200

    feat: add routerlink (id, detail page)

--- src/app/shared/components/product-detail/product-detail.ts
+++ src/app/shared/components/product-detail/product-detail.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,17 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './product-detail.scss',
 })
 export class ProductDetail {
+
+  private route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    let currentName = this.route.snapshot.paramMap.get('name');
+    if (currentName) this.detail.name = currentName;
+  }
+
   detail = {
-      name: 'Gaming Maus',
-      description: `Eine ergonomische Gaming-Maus mit hoher Praezision und einstellbarer
+    name: 'Gaming Maus',
+    description: `Eine ergonomische Gaming-Maus mit hoher Praezision und einstellbarer
 DPI. Ideal fuer FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortable
 Seitentasten fuer schnelles Reagieren.`,
-      specs: 'dpi: 6400, cable length: 1.8m, color: Schwarz',
-      stock: 120,
-      price: 2500000,
-    }
+    specs: 'dpi: 6400, cable length: 1.8m, color: Schwarz',
+    stock: 120,
+    price: 2500000,
+  }
 
-    deleteDetail(){
-      this.detail.name = "";
-    }
+  deleteDetail() {
+    this.detail.name = "";
+  }
 }
